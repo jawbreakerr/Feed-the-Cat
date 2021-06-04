4922b6e66a319fbc051e1f3fa056c3b03a2330c4
ignacio arguello <jawbreaker.iaa@gmail.com>
2021-06-04 13:09:25 -0300

    Revert "Nivel 1 terminado"
    This reverts commit 91dc4695af05b43d25bab2c2d1a7c89b2717b96d.

--- Assets/Scripts/Dinamica.cs
+++ Assets/Scripts/Dinamica.cs
@@ -1,62 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;   
 
 public class Dinamica : MonoBehaviour
 {
     #region Variables
-    public GameObject Celular, Billetera, Campera, Barbijo, BarbijoB, CelularB, BilleteraB, CamperaB;
-    public int PickedObjects;
+    public GameObject Celular, Billetera, Campera, CelularB, BilleteraB, CamperaB;
     #endregion
     void Start()
     {
-        //para descomentar ctrl k u
-        //para comentar ctrl k c
-        //Celular.SetActive(false);
-        //Billetera.SetActive(false);
-        //Campera.SetActive(false);
-        //Barbijo.SetActive(false);
+        Celular.SetActive(false);
+        Billetera.SetActive(false);
+        Campera.SetActive(false);
         CelularB.SetActive(true);
         BilleteraB.SetActive(true);
         CamperaB.SetActive(true);
-        BarbijoB.SetActive(true);
-        PickedObjects = 0;
-    }
-    void Update()
-    {
 
     }
-    #region BotonesObjetos
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
     public void Celu()
     {
-        //Celular.SetActive(true);
+        Celular.SetActive(true);
         CelularB.SetActive(false);
-        PickedObjects += 1;
 
     }
     public void Bitellera()
     {
-        //Billetera.SetActive(true);
+        Billetera.SetActive(true);
         BilleteraB.SetActive(false);
-        PickedObjects += 1;
+
     }
     public void Campe()
     {
-        //Campera.SetActive(true);
+        Campera.SetActive(true);
         CamperaB.SetActive(false);
-        PickedObjects += 1;
     }
-    public void Barbijillo()
-    {
-        //Barbijo.SetActive(true);
-        BarbijoB.SetActive(false);
-        PickedObjects += 1;
-    }
-    public void Puertirigilla()
-    {
-        if (PickedObjects == 4)
-        {
-            SceneManager.LoadScene("Nivel 2");
-        }
-    }
-    #endregion
 }
